188cf333c693c9e4c03680fe7a23b832c897c46e
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-27 19:04:30 +0800

    optimize authorization

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { AuthUserContext, withAuthorization } from '../Session';
 import PasswordChangeForm from '../PasswordChange';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
