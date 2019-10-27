7c49ee3b95ca2fe776d066d3a0a988c6c2ead666
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-27 17:34:23 +0800

    setup forget password

--- src/components/PasswordForget/index.jsx
+++ src/components/PasswordForget/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
-const PasswordForget = () => (
+import { Link } from 'react-router-dom';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import PasswordForgetFormBase from './PasswordForget';
+const PasswordForgetPage = () => (
   <div>
     <h1>PasswordForget</h1>
+    <PasswordForgetForm />
   </div>
 );
-export default PasswordForget;
+
+const PasswordForgetLink = () => (
+  <p>
+    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+  </p>
+);
+export default PasswordForgetPage;
+
+const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
+export { PasswordForgetForm, PasswordForgetLink };
--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -4,10 +4,12 @@ import { compose } from 'recompose';
 import { withFirebase } from '../Firebase';
 import { SignUpLink } from '../SignUp';
 import SignInFormBase from './SignInForm';
+import { PasswordForgetLink } from '../PasswordForget';
 const SignInPage = () => (
   <div>
     <h1>SignIn</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
