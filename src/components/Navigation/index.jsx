3f9bc361e8a50e675399a831a91e1bdfbd3c9039
Chelzea Salvador <chelzeasalvador@gmail.com>
2019-10-27 17:25:19 +0800

    optimize code

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import { AuthUserContext } from '../Session';
 
 import SignOutButton from '../SignOut';
 
-const Navigation = ({ authUser }) => (
-  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+const Navigation = () => (
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+    </AuthUserContext.Consumer>
+  </div>
 );
 const NavigationAuth = () => (
   <ul>
